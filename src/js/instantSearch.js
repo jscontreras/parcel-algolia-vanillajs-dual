aab6b66ce55d2dabf7d03cdadecc747f2913af07
Sebastian Contreras <sebastian.contreras@algolia.com>
2023-07-20 15:56:37 -0500

    Adding default index dynamic config.

--- src/js/instantSearch.js
+++ src/js/instantSearch.js
@@ -61,7 +61,7 @@ const myInstantSearchGlobalConfig = configure({
 const sortByWidget = sortBy({
   container: '#sort-by__container',
   items: [
-    { label: 'Featured', value: 'instant_search' },
+    { label: 'Featured', value: searchConfig.recordsIndex },
     { label: 'Price (asc)', value: 'instant_search_price_asc' },
     { label: 'Price (desc)', value: 'instant_search_price_desc' },
   ],
